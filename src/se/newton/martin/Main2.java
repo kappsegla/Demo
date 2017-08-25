944bf510fdf294f6085f172a250e71c74a610a3c
Martin Blomberg <martin.blomberg@outlook.com>
2017-08-25 13:13:12 +0200

    MenyAlternativ
    Switchsats för val av menyalternativ

--- src/se/newton/martin/Main2.java
+++ src/se/newton/martin/Main2.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class Main2 {
 
 
-
-    public static void gissatalet()
-    {
+    public static void gissatalet() {
         int secretNumber = (int) (Math.random() * 50) + 1;
         int yourGuess = 0;
 
@@ -15,12 +13,12 @@ public class Main2 {
 
         //for( int i = 1; yourGuess != secretNumber; i++) {
         int i = 1;
-        while(yourGuess != secretNumber){
+        while (yourGuess != secretNumber) {
             System.out.println("Välj ett tal mellan 1-50");
             yourGuess = sc.nextInt();
 
             if (yourGuess == secretNumber)
-                System.out.println("Grattis, du gissade rätt på " + i +" försök!");
+                System.out.println("Grattis, du gissade rätt på " + i + " försök!");
             else if (yourGuess > secretNumber)
                 System.out.println("För stort");
             else
@@ -29,42 +27,50 @@ public class Main2 {
         }
     }
 
-    public static void utskriftermedloop()
-    {
+    public static void utskriftermedloop() {
         //Skriver ut: 10 20 30 40 50 60 70 80 90 100
-        for(int r = 1; r <= 10; r++)
-        {
+        for (int r = 1; r <= 10; r++) {
             System.out.println(r * 10);
         }
         //Skriver ut baklänges: 10 9 8 ... 0
-        for(int r = 10; r >= 0; r--)
-        {
+        for (int r = 10; r >= 0; r--) {
             System.out.println(r);
         }
 
         //Skriver ut alfabetet a till z med små bokstäver
         char c = 'a';
-        while(c < 'z' + 1)
-        {
+        while (c < 'z' + 1) {
             System.out.print(c);
             c++;
         }
     }
 
-    public static int readANumber()
-    {
+    public static int readANumber() {
         Scanner sc = new Scanner(System.in);
         int temp = sc.nextInt();
-        sc.close();
         return temp;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
+        while (true) {
+            System.out.println("1. Gissa talet");
+            System.out.println("2. Utskrift med loopar");
+            System.out.println("0. Avsluta");
 
+            switch (readANumber()) {
+                case 0:
+                    return;
+                case 1:
+                    gissatalet();
+                    break;
+                case 2:
+                    utskriftermedloop();
+                    break;
+                default:
+                    System.out.println("Ej giltigt val");
+            }
 
-        gissatalet();
-
-        utskriftermedloop();
+        }
     }
 }
