26eb4345312838c890cc1de3427c753d3a124234
Martin Blomberg <martin.blomberg@outlook.com>
2017-09-06 13:13:53 +0200

    a

--- src/se/newton/martin/animalgame/AnimalGame.java
+++ src/se/newton/martin/animalgame/AnimalGame.java
@@ -1,13 +1,34 @@
 package se.newton.martin.animalgame;
 
+import java.util.ArrayList;
+
 public class AnimalGame {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        ArrayList<Mammal> animals = new ArrayList<>();
+
+        animals.add( new Cat() );
+        animals.add( new Cat() );
+        animals.add( new Dog() );
+        animals.add( new Cat() );
+        animals.add(new Duck());
+
+        for (int i = 0; i < animals.size() ; i++) {
+            animals.get(i).talk();
+        }
+
+
+
+
+
+        /*
+        Mammal cat = new Cat();
         cat.talk();
 
-        Dog dog = new Dog();
-        dog.talk();
+        Mammal dog = new Dog();
+        dog.talk();*/
+
+
 
     }
 }
---
+++ src/se/newton/martin/animalgame/Duck.java
@@ -0,0 +1,8 @@
+package se.newton.martin.animalgame;
+
+public class Duck extends Mammal {
+    @Override
+    public void talk() {
+        System.out.println("Quack");
+    }
+}
