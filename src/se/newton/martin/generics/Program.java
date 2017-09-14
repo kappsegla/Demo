67a512a3a41864ff93a6dcb4f33eeb06a71b94d9
Martin Blomberg <martin.blomberg@outlook.com>
2017-09-14 14:25:50 +0200

    Generic class and Generic Method.

--- src/se/newton/martin/generics/Program.java
+++ src/se/newton/martin/generics/Program.java
@@ -1,13 +1,28 @@
 package se.newton.martin.generics;
 
-public class Program {
-    public static void  main(String[] args) {
+import se.newton.martin.ovning3.shapes.Rectangle;
+import se.newton.martin.ovning3.shapes.Shape;
 
+public class Program {
+
+    /**
+     * Javadoc comment. This is a generic method that can take any reference as input.
+     * Type is decided at compiletime.
+     * @param arg
+     * @param <T>
+     */
+    public static <T> void genericMethod(T arg) {
+        System.out.println(arg);
+    }
+
+    public static void  main(String[] args) {
         StorageBin<Integer> placeToStoreThings = new StorageBin<>();
         placeToStoreThings.setStorage(10);
         placeToStoreThings.add(20);
-        
 
+        genericMethod("Test");
+        genericMethod(345);
+        genericMethod(new Rectangle(10,10));
 
     }
 }
