5d97823992c5de12111e3539cb5cc71ddf4aca88
Martin Blomberg <martin.blomberg@outlook.com>
2017-09-21 14:32:26 +0200

    FileChooser

--- src/se/newton/martin/gui/GUIExempel.java
+++ src/se/newton/martin/gui/GUIExempel.java
@@ -7,6 +7,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.util.ResourceBundle;
 
 public class GUIExempel {
     private JPanel root;
@@ -19,29 +21,11 @@ public class GUIExempel {
     private JMenuBar menuBar;
     private static JFrame frame;
 
+    private static ResourceBundle bundle = ResourceBundle.getBundle("se.newton.martin.gui.textstrings");
+
     public GUIExempel() {
-        menuBar = new JMenuBar();
-        frame.setJMenuBar(menuBar);
-        JMenu menu = new JMenu("Meny");
-        menu.setMnemonic('M');
-        menuBar.add(menu);
-
-
-        //Keyevent is used for navigating with alt + underscored character in windows
-        JMenuItem menuItem = new JMenuItem("A text-only menu item",
-                KeyEvent.VK_T);
-
-
-        menuItem.setAccelerator(KeyStroke.getKeyStroke(
-                KeyEvent.VK_1, ActionEvent.ALT_MASK));
-
-        menuItem.getAccessibleContext().setAccessibleDescription(
-                "This doesn't really do anything");
-        menu.add(menuItem);
-
-        menuItem.addActionListener( (e) -> {
-            myListModel.add(textField1.getText());
-        });
+        //Create menuBar for the application
+        createMenuBar();
 
         myListModel = new MyListModel();
         list1.setModel(myListModel);
@@ -61,16 +45,81 @@ public class GUIExempel {
         list1.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
-                if( e.getValueIsAdjusting() == false){
+                if (e.getValueIsAdjusting() == false) {
                     //Selection/Deselection is done
-                    if( list1.getSelectedIndex() == -1){
+                    if (list1.getSelectedIndex() == -1) {
                         taBortButton.setEnabled(false);
-                    }
-                    else
+                    } else
                         taBortButton.setEnabled(true);
                 }
             }
         });
+    }
+
+    private void createMenuBar() {
+        menuBar = new JMenuBar();
+        frame.setJMenuBar(menuBar);
+        JMenu menu = new JMenu("File");
+        //Access the menu with alt+F
+        menu.setMnemonic('F');
+        //Lägg File menu på menubar
+        menuBar.add(menu);
+        //Keyevent is used for navigating with alt + underscored character in windows
+        JMenuItem openFileMenu = new JMenuItem(bundle.getString("fileOpen"),
+                KeyEvent.VK_O);
+
+        openFileMenu.getAccessibleContext().setAccessibleDescription(
+                "Select a file to open");
+        menu.add(openFileMenu);
+
+        openFileMenu.addActionListener(this::openFile);
+
+        JMenuItem saveFileMenu = new JMenuItem("Save...",
+                KeyEvent.VK_S);
+
+        saveFileMenu.getAccessibleContext().setAccessibleDescription(
+                "Save to file");
+        saveFileMenu.setAccelerator(KeyStroke.getKeyStroke(
+                KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        menu.add(saveFileMenu);
+
+        saveFileMenu.addActionListener(this::saveFile);
+    }
+
+    public void openFile(ActionEvent e) {
+        //Create a file chooser
+        final JFileChooser fc = new JFileChooser();
+
+        //In response to a button click. showOpenDialog returns when the dialog closes.
+        int returnVal = fc.showOpenDialog(frame);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+
+            //Filhantering tar lång tid och måste utföras i en egen bakgrundstråd.
+            //Kan även göras med SwingWorker
+            // https://docs.oracle.com/javase/8/docs/api/javax/swing/SwingWorker.html
+            new Thread(() -> {
+                //Läs in filen
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+                //Uppdatera GUI, på EDT tråden.
+                EventQueue.invokeLater(() -> {
+                    //Kod för att uppdatera grafiska gränssnittet
+                    textField2.setText(file.getName());
+                });
+            }).start();
+        }
+    }
+
+    public void saveFile(ActionEvent e) {
+        //Create a file chooser
+        final JFileChooser fc = new JFileChooser();
+
+        //In response to a button click:
+        int returnVal = fc.showSaveDialog(null);
     }
 
     public static void main(String[] args) {
@@ -78,7 +127,7 @@ public class GUIExempel {
             frame = new JFrame("GUIExempel");
             frame.setContentPane(new GUIExempel().root);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(new Dimension(800,600));
+            frame.setSize(new Dimension(800, 600));
             frame.setVisible(true);
         });
     }
